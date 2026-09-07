16966b2b0cc66a2fd1f2d71a30385fd8e94d6d27
hxl1995zz <1247594616@qq.com>
2026-09-07 22:30:58 +0800

    1

--- example/paint.py
+++ example/paint.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-df=pd.read_csv('300_1h_50_money.csv.best')
+df=pd.read_csv('300_1h_50_spot_0.001_money.csv')
 # 常用参数：kind (图表类型), x (横轴), y (纵轴), title (标题)
-df.plot(kind='line', x='date', y='money', title='示例图表')
+df.plot(kind='line', x='date', y=['money','clear_money'],secondary_y=['clear_money'], title='示例图表')
 plt.show()
