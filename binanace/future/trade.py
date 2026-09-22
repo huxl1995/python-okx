71dc40d1b78db731f058c58a982cada5f59ea15b
hxl <1247594515@qq.com>
2026-09-23 00:05:44 +0800

    重构代码

--- binanace/future/trade.py
+++ binanace/future/trade.py
@@ -9,7 +9,7 @@ from binance_sdk_derivatives_trading_usds_futures.derivatives_trading_usds_futur
     DERIVATIVES_TRADING_USDS_FUTURES_REST_API_PROD_URL,
 )
 from binance_sdk_derivatives_trading_usds_futures.rest_api.models import (
-    NewOrderSideEnum,
+    NewOrderSideEnum, NewAlgoOrderSideEnum,
 )
 from dotenv import load_dotenv
 
@@ -68,8 +68,45 @@ def cancel_all_open_orders(symbol):
         logging.info(f"cancel_all_open_orders() response: {data}")
     except Exception as e:
         logging.error(f"cancel_all_open_orders() error: {e}")
+def new_algo_order(symbol,side,trigger_price):
+    try:
+        response = client.rest_api.new_algo_order(
+            algo_type="CONDITIONAL",
+            symbol=symbol,
+            side=NewAlgoOrderSideEnum[side].value,
+            type="STOP_MARKET",
+            working_type="MARK_PRICE",
+            close_position="true",
+            trigger_price=trigger_price,
+            time_in_force="GTC"
+        )
 
+        rate_limits = response.rate_limits
+        logging.info(f"new_algo_order() rate limits: {rate_limits}")
+
+        data = response.data()
+        logging.info(f"new_algo_order() response: {data}")
+    except Exception as e:
+        logging.error(f"new_algo_order() error: {e}")
+def cancel_all_algo_open_orders(symbol):
+    try:
+        response = client.rest_api.cancel_all_algo_open_orders(
+            symbol=symbol,
+        )
+
+        rate_limits = response.rate_limits
+        logging.info(f"cancel_all_algo_open_orders() rate limits: {rate_limits}")
+
+        data = response.data()
+        logging.info(f"cancel_all_algo_open_orders() response: {data}")
+    except Exception as e:
+        logging.error(f"cancel_all_algo_open_orders() error: {e}")
+def trailing_stop_market(symbol):
+    response = client.rest_api.position_information_v3(symbol)
+    data=response.data()
+    print(1)
 if __name__=="__main__":
     #new_order("BTCUSDT","BUY","MARKET",1)
     #new_market_order("BTCUSDT","SELL",0.01)
-    new_limit_order("BTCUSDT","SELL",0.01,81000)
+    #new_algo_order("BTCUSDT","SELL",81000)
+    trailing_stop_market("BTCUSDT")
