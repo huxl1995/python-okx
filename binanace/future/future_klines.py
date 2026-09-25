0717e3919f5d08f471c8b051b503d12a5dd157d7
hxl <1247594515@qq.com>
2026-09-25 14:13:15 +0800

    重构代码

--- binanace/future/future_klines.py
+++ binanace/future/future_klines.py
@@ -87,12 +87,12 @@ def klines(
 
 
 if __name__ == "__main__":
-    end_time=datetime(2025,1,20,0,0,0)
+    end_time=datetime(2026,1,1,21,0,0)
     dataframe=pd.DataFrame()
-    while end_time<datetime(2026,9,16,22,0,0):
+    while end_time<datetime(2026,1,11,21,0,0):
         data = fetch_klines(interval="1m", start_time=int(end_time.timestamp())*1000,end_time=int((end_time+timedelta(hours=15)).timestamp()) * 1000)
         dataframe=pd.concat([dataframe,data],ignore_index=True)
         end_time = end_time + timedelta(hours=15)
+        dataframe.to_csv("klines_1m.csv.part2")
         sleep(1)
-    dataframe.to_csv("klines_1m.csv")
-    logging.info("klines sample:\n%s", data.tail())
+    logging.info("klines sample:\n%s", dataframe.tail())
